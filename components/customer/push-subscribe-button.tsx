0954dc0b3b89200af98a7433a2ac762b0d4da102
jaenam <jaenam1212@gmail.com>
2026-02-13 21:06:24 +0900

    4

--- components/customer/push-subscribe-button.tsx
+++ components/customer/push-subscribe-button.tsx
@@ -8,11 +8,11 @@ import toast from 'react-hot-toast'
 
 const VAPID_PUBLIC_KEY = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
-  const output = new Uint8Array(rawData.length)
+  const output = new Uint8Array(new ArrayBuffer(rawData.length))
   for (let i = 0; i < rawData.length; ++i) output[i] = rawData.charCodeAt(i)
   return output
 }
